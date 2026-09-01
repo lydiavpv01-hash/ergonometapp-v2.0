c8a7f95754a3070db4f4d4d06a33feb6e46d5f4a
Ing. Lydia Peralta <lydia@concienciaverde.com.mx>
2026-09-01 18:38:22 +0000

    Feature: REBA v2 Completa - Datos de persona + Cálculo en inicio + Imágenes lado a lado + Kinovea integrado

--- routes/metodos/reba.py
+++ routes/metodos/reba.py
@@ -24,7 +24,7 @@ def nueva():
             'puntuacion': 0
         })
     
-    return render_template('reba_interactivo.html')
+    return render_template('reba_v2_completa.html')
 
 @bp_reba.route('/resultado/<int:id>')
 @login_required

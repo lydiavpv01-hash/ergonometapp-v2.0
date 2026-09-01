6b17f5b7348e68ead4deb62c701248b55a6947cb
Ing. Lydia Peralta <lydia@concienciaverde.com.mx>
2026-09-01 22:23:02 +0000

    Feature: REBA v5 - Descarga PDF Profesional con TODOS LOS AJUSTES incluidos (cuello torcido, torso inclinado, hombro levantado, etc.)

--- routes/metodos/reba.py
+++ routes/metodos/reba.py
@@ -24,7 +24,7 @@ def nueva():
             'puntuacion': 0
         })
     
-    return render_template('reba_v4_preciso.html')
+    return render_template('reba_v5_pdf_profesional.html')
 
 @bp_reba.route('/resultado/<int:id>')
 @login_required

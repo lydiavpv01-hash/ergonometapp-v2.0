3e055238628a2f9a00d63d0a4d03c2d81fe8a130
Ing. Lydia Peralta <lydia@concienciaverde.com.mx>
2026-09-01 18:51:41 +0000

    Feature: REBA v3 - Kinovea FUNCIONAL integrado (canvas real con cálculo de ángulos en vivo)

--- routes/metodos/reba.py
+++ routes/metodos/reba.py
@@ -24,7 +24,7 @@ def nueva():
             'puntuacion': 0
         })
     
-    return render_template('reba_v2_completa.html')
+    return render_template('reba_v3_kinovea.html')
 
 @bp_reba.route('/resultado/<int:id>')
 @login_required

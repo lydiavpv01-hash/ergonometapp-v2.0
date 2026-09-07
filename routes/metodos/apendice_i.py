0581f2880c4ac72e3ac60383271f9fa2f7e3f88f
lydiavpv01-hash <lydiavpv01@gmail.com>
2026-09-07 11:34:18 -0600

    feat: activate Appendix I precision refinements

--- routes/metodos/apendice_i.py
+++ routes/metodos/apendice_i.py
@@ -60,5 +60,6 @@ AI5_ENHANCEMENT = r"""
 def nueva():
     html = render_template('apendice_i_workspace.html')
     intro = render_template('apendice_i_intro.html')
+    refinements = render_template('apendice_i_refinements_v3.html')
     html = html.replace("<div class='tabs'>", intro + "<div class='tabs'>", 1)
-    return html.replace('</body>', AI5_ENHANCEMENT + '</body>')
+    return html.replace('</body>', AI5_ENHANCEMENT + refinements + '</body>')

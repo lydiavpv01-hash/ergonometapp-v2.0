ff798d6e2f5ce2f2557db16596009505ddeab464
lydiavpv01-hash <lydiavpv01@gmail.com>
2026-09-07 13:50:08 -0600

    feat: inject guided Apéndice II refinements

--- routes/metodos/apendice_ii.py
+++ routes/metodos/apendice_ii.py
@@ -4,4 +4,6 @@ bp_apendice_ii = Blueprint('apendice_ii', __name__, url_prefix='/apendice-ii')
 
 @bp_apendice_ii.route('/nueva', methods=['GET'])
 def nueva():
-    return render_template('apendice_ii_workspace.html')
+    html = render_template('apendice_ii_workspace.html')
+    refinements = render_template('apendice_ii_refinements_v1.html')
+    return html.replace('</body>', refinements + '</body>')

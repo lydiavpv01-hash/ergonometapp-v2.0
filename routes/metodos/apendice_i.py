2ae1731c461550fd6c53ba5bee69a7d460c5b45e
lydiavpv01-hash <lydiavpv01@gmail.com>
2026-09-07 02:16:35 -0600

    feat: implement NOM-036 Appendix I workspace

--- routes/metodos/apendice_i.py
+++ routes/metodos/apendice_i.py
@@ -1,7 +1,7 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, render_template
 
 bp_apendice_i = Blueprint('apendice_i', __name__, url_prefix='/apendice-i')
 
-@bp_apendice_i.route('/nueva', methods=['GET', 'POST'])
+@bp_apendice_i.route('/nueva', methods=['GET'])
 def nueva():
-    return jsonify({'método': 'Apéndice I', 'estado': 'en desarrollo'})
+    return render_template('apendice_i_workspace.html')

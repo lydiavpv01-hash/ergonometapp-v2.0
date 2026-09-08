300a3e09c8d43655b671332f4db493211b2b5492
lydiavpv01-hash <lydiavpv01@gmail.com>
2026-09-08 11:43:12 -0600

    feat: inject matrix metadata and logo uploads in NOM evaluations

--- app.py
+++ app.py
@@ -81,8 +81,13 @@ def after_request(response):
             if 'factor-evidence-title' not in html:
                 html = html.replace('</body>', refinement + '</body>')
 
+        if request.path in ('/apendice-i/nueva','/apendice-ii/nueva'):
+            matrix_meta = render_template('nom_matrix_metadata.html')
+            if 'matrixClientData' not in html:
+                html = html.replace('</body>', matrix_meta + '</body>')
+
         if request.path in ('/apendice-i/nueva','/apendice-ii/nueva','/cuestionario-nordico/nueva'):
-            script = '<script src="/static/js/nom_persistence.js?v=2"></script>'
+            script = '<script src="/static/js/nom_persistence.js?v=3"></script>'
             if script not in html:
                 html = html.replace('</body>', script + '</body>')
 
